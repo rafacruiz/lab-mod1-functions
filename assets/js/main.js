d39aad6c9cc578b0a493a4816baceb084ca31226
Carlos del Prado <carlos.delpradomota@telefonica.com>
2025-10-11 09:00:47 +0200

    feat: base lab fns

---
+++ assets/js/main.js
@@ -0,0 +1,104 @@
+/**
+ * Check if a number is even.
+ * @param {number} number
+ * @returns {boolean}
+ */
+function isEven(number) {
+
+}
+
+/**
+ * Count the number of characters in a string.
+ * @param {string} text
+ * @returns {number}
+ */
+function getLength(text) {
+
+}
+
+/**
+ * Capitalize the first letter of a word and keeps the others in lower case.
+ * @param {string} word
+ * @returns {string}
+ */
+function capitalize(word) {
+
+}
+
+/**
+ * Count how many even numbers are in an array.
+ * @param {number[]} numbers
+ * @returns {number}
+ */
+function countEvens(numbers) {
+
+}
+
+
+/**
+ * Repeat the given word a specified number of times.
+ * @param {string} word - The word to repeat.
+ * @param {number} times - How many times to repeat it.
+ * @returns {string}
+ */
+function repeat(word, times) {
+
+}
+
+/**
+ * Reverse the characters in a string.
+ * @param {string} text
+ * @returns {string}
+ */
+function reverse(text) {
+
+}
+
+/**
+ * Check if a element exists in an array.
+ * @param {(string|number|boolean)[]} elements
+ * @param {string|number|boolean} element
+ * @returns {boolean}
+ */
+function includes(elements, element) {
+
+}
+
+/**
+ * Returns words from the array that are longer than the specified number of characters.
+ * @param {string[]} words - The list of words to filter.
+ * @param {number} minLength - Minimum number of characters a word must have to be included.
+ * @returns {string[]} - Array of words longer than minLength.
+ */
+function getWordsLongerThan(words, minLength) {
+
+}
+
+/**
+ * Return a new array with numbers greater than minValue.
+ * @param {number[]} numbers
+ * @param {number} minValue
+ * @returns {number[]}
+ */
+function filterGreaterThan(numbers, minValue) {
+
+}
+
+/**
+ * Remove all duplicate spaces from a string.
+ * @param {string} text
+ * @returns {string}
+ */
+function cleanDuplicateSpaces(text) {
+
+}
+
+/**
+ * Join an array of words using 'separator'.
+ * @param {string[]} words
+ * @param {string} separator
+ * @returns {string}
+ */
+function joinWith(words, separator) {
+
+}
